b21b98018f78d816dfd09712320006e1acccbf47
vietvtc4 <vietvtc4@gmail.com>
2025-07-06 18:53:24 +0700

    del-Next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/vietvtc4/Documents/Farcoins-Hackathon/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
